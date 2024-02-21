63020d65085344de104004c729deae4537e97933
rajkbarvadiya <160387854+rajkbarvadiya@users.noreply.github.com>
2024-02-21 23:15:42 +0530

    Update logic half_adder.v

--- half_adder.v
+++ half_adder.v
@@ -5,8 +5,4 @@ module half_adder(a,b,sum,carry);
 input a,b; //input
 output sum,carry;//output
 //reg sum,carry;//output
-
-//gate level modeling
-xor x1(sum,a,b); //sum
-and a1(carry,a,b);//carry
 endmodule
