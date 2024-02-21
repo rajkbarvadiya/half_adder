2d31125fa2eacb729423087d5c143547a1d745b3
rajkbarvadiya <160387854+rajkbarvadiya@users.noreply.github.com>
2024-02-21 23:04:19 +0530

    Update for task half_adder.v

--- half_adder.v
+++ half_adder.v
@@ -9,9 +9,4 @@ output sum,carry;//output
 //gate level modeling
 xor x1(sum,a,b); //sum
 and a1(carry,a,b);//carry
-
-//data flow level modeling
-/* assign sum = a ^ b; //sum
-assign carry = a & b; */  //carry
-
 endmodule
